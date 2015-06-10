8e7c002fcca8edc2d378f562051e9b9b949dddbd
QuentinV <quentvht@hotmail.com>
2015-06-11 01:33:08 +0200

    tests

---
+++ ISI3 Project/test/model/authorizer/AuthPafPafTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.authorizer;
+
+import model.SurfaceType;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Necrolight
+ */
+public class AuthPafPafTest {
+    
+    public AuthPafPafTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getAllowedSurfaceTypes method, of class AuthPafPaf.
+     */
+    @Test
+    public void testGetAllowedSurfaceTypes() {
+        System.out.println("getAllowedSurfaceTypes");
+        AuthPafPaf instance = new AuthPafPaf();
+        SurfaceType[] expResult = new SurfaceType[] { SurfaceType.Plat, SurfaceType.Escarpe };
+        SurfaceType[] result = instance.getAllowedSurfaceTypes();
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of canGoThroughWater method, of class AuthPafPaf.
+     */
+    @Test
+    public void testCanGoThroughWater() {
+        System.out.println("canGoThroughWater");
+        AuthPafPaf instance = new AuthPafPaf();
+        Boolean expResult = false;
+        Boolean result = instance.canGoThroughWater();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of canGoThroughFire method, of class AuthPafPaf.
+     */
+    @Test
+    public void testCanGoThroughFire() {
+        System.out.println("canGoThroughFire");
+        AuthPafPaf instance = new AuthPafPaf();
+        Boolean expResult = false;
+        Boolean result = instance.canGoThroughFire();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toString method, of class AuthPafPaf.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        AuthPafPaf instance = new AuthPafPaf();
+        String expResult = "Paf paf";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+    
+}
---
+++ ISI3 Project/test/model/authorizer/AuthSnapSnapTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.authorizer;
+
+import model.SurfaceType;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Necrolight
+ */
+public class AuthSnapSnapTest {
+    
+    public AuthSnapSnapTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getAllowedSurfaceTypes method, of class AuthSnapSnap.
+     */
+    @Test
+    public void testGetAllowedSurfaceTypes() {
+        System.out.println("getAllowedSurfaceTypes");
+        AuthSnapSnap instance = new AuthSnapSnap();
+        SurfaceType[] expResult = new SurfaceType[] { SurfaceType.Plat };
+        SurfaceType[] result = instance.getAllowedSurfaceTypes();
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of canGoThroughWater method, of class AuthSnapSnap.
+     */
+    @Test
+    public void testCanGoThroughWater() {
+        System.out.println("canGoThroughWater");
+        AuthSnapSnap instance = new AuthSnapSnap();
+        Boolean expResult = false;
+        Boolean result = instance.canGoThroughWater();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of canGoThroughFire method, of class AuthSnapSnap.
+     */
+    @Test
+    public void testCanGoThroughFire() {
+        System.out.println("canGoThroughFire");
+        AuthSnapSnap instance = new AuthSnapSnap();
+        Boolean expResult = false;
+        Boolean result = instance.canGoThroughFire();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toString method, of class AuthSnapSnap.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        AuthSnapSnap instance = new AuthSnapSnap();
+        String expResult = "Snap snap";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+    
+}
